7d283cbb94d0ccdd80c64b0b04b3b7de3c05af3c
Artem Netsvetaev <cobal2m@gmail.com>
2025-11-28 15:28:32 +0300

    prettier and eslint

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,9 @@
-import type { ClassValue } from "clsx"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import type { ClassValue } from "clsx";
 
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
